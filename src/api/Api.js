1d30adc509f0436b267f4d363fee650f42bf4699
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-01-10 17:00:27 +0100

    fetched data from figma api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,16 +1,17 @@
+// import axios from 'axios';
 
-import axios from 'axios'
-const fetchData = async () => {
-  try {
-    const response = await axios.get('https://api.figma.com/v1/files/vRCqsniN1t2PndqlKeYQwI', {
-      headers: {
-        'X-Figma-Token': 'figd_dN1neMPKHHmnDFU4AFlSMPwv6ZqLqK_ANxk7hj3l'
-      }
-    });
-    return response.data
-  } catch (error) {
-    console.error(error);
-  }
-}
+// const fetchData = async (url, token) => {
+//   try {
+//     const response = await axios.get(url, {
+//       headers: {
+//         'X-Figma-Token': token
+//       }
+//     });
+//     return response.data
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
 
-export default fetchData
+// export default fetchData
